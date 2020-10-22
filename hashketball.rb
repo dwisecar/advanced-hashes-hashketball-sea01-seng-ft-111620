a178a0e7550a61255773387145e97fa9d5ed9afc
David Wisecarver <dwisecar@gmail.com>
2020-10-22 20:26:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,8 +201,9 @@ def player_stats(player)
   game_hash.each do |key, value|
     value[:players].each do |player_hash|
       if player == player_hash[:player_name]
-        #this_player_stats = player_hash
+        this_player_stats = player_hash
       end
+    end
   end  
   
   this_player_stats
@@ -211,19 +212,3 @@ end
 def big_shoe_rebounds
   
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
